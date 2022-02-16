6e160a716d99b569e418d02e59a723aa9572a232
raheemadamboev <raheemideadev@gmail.com>
2022-02-16 12:58:56 +0500

    ui and binding

--- app/src/main/java/xyz/teamgravity/kotlincoroutines5mistakes/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/kotlincoroutines5mistakes/MainActivity.kt
@@ -2,11 +2,15 @@
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import xyz.teamgravity.kotlincoroutines5mistakes.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
