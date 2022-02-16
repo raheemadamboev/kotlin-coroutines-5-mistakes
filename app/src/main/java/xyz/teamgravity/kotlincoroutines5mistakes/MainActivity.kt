c9c96c90028d9a4e0bd7934fb4f1f5a3b70680d9
raheemadamboev <raheemideadev@gmail.com>
2022-02-16 12:54:42 +0500

    initial

---
+++ app/src/main/java/xyz/teamgravity/kotlincoroutines5mistakes/MainActivity.kt
@@ -0,0 +1,12 @@
+ package xyz.teamgravity.kotlincoroutines5mistakes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
